acca49dfacc2fc77bf894bcda3f6362f6000ffd6
boris.hernandez <bhernandez@applaudostudios.com>
2017-12-07 11:22:28 -0600

    Initial commit Create React App and Bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Board from './board';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        React App!
+      </div>
+    );
+  }
+}
+
+export default App;
